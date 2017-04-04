1ee4510432e0b0381181747739f19bbfe189c1c5
jinpangleung <leung@udel.edu>
2017-04-04 19:17:15 -0400

    Update 4to1Mux.vhdl
    Added comments

--- behavioralModels/4to1Mux.vhdl
+++ behavioralModels/4to1Mux.vhdl
@@ -3,12 +3,12 @@ use ieee.std_logic_1164.all;
 
 entity fourToOne is
 generic ( N : integer := 8);
-port(	wordOne: in std_logic_vector (N-1 downto 0);
-	wordTwo: in std_logic_vector (N-1 downto 0);
-	wordThree: in std_logic_vector (N-1 downto 0);
-	wordFour: in std_logic_vector (N-1 downto 0);
-	sel: in std_logic_vector (1 downto 0);
-	output: out std_logic_vector (N-1 downto 0)
+port(	wordOne: in std_logic_vector (N-1 downto 0); --first word
+	wordTwo: in std_logic_vector (N-1 downto 0); --second word
+	wordThree: in std_logic_vector (N-1 downto 0); --third word
+	wordFour: in std_logic_vector (N-1 downto 0); --fourth word
+	sel: in std_logic_vector (1 downto 0); --select
+	output: out std_logic_vector (N-1 downto 0) --output
 );
 end entity fourToOne;
 
